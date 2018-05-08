f72025bb5a96246a80487efffd8e6503ab91c031
Noman Sanaullah <32468765+nomans1@users.noreply.github.com>
2018-05-09 00:29:55 +0100

    Update email.php

--- php-modules/email.php
+++ php-modules/email.php
@@ -8,8 +8,8 @@ $mail = new PHPMailer;
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'dummyphp1@gmail.com';                 // SMTP username
-$mail->Password = 'php123456789';                           // SMTP password
+$mail->Username = '';                 // SMTP username
+$mail->Password = '';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to //587
 
